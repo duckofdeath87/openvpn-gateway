548dd6fff1a2ffb01168d9a4d352349536a1a83a
duckofdeath87 <duckofdeath87@gmail.com>
2019-12-02 20:42:26 -0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,7 +8,7 @@
 /usr/sbin/iptables -I INPUT -i eth0 -j ACCEPT
 
 # Out to VPN
-/usr/sbin/iptables -I OUTPUT -o tun+ -j ACCEPT
+/usr/sbin/iptables -I OUTPUT -o tun0 -j ACCEPT
 
 # OpenVPN
 /usr/sbin/iptables -A OUTPUT -o eth0 -p udp --dport 1198 -j ACCEPT
@@ -22,10 +22,10 @@
 # dns
 /usr/sbin/iptables -A OUTPUT -o eth0 -p udp --dport 53 -j ACCEPT
 
-/usr/sbin/iptables -A FORWARD -i tun+ -o eth0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+/usr/sbin/iptables -A FORWARD -i tun0 -o eth0 -m state --state RELATED,ESTABLISHED -j ACCEPT
 
 # LAN out to VPN
-/usr/sbin/iptables -A FORWARD -i eth0 -o tun+ -j ACCEPT
-/usr/sbin/iptables -t nat -A POSTROUTING -o tun+ -j MASQUERADE
+/usr/sbin/iptables -A FORWARD -i eth0 -o tun0 -j ACCEPT
+/usr/sbin/iptables -t nat -A POSTROUTING -o tun0 -j MASQUERADE
 
 /usr/sbin/openvpn --config /etc/openvpn/${VPNCONF:-vpn.conf}

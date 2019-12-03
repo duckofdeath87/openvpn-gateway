ad628ac90f87fb858fba6789c59904226b71b88f
duckofdeath87 <duckofdeath87@gmail.com>
2019-12-02 18:44:45 -0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,17 @@
-iptables -A INPUT -i lo -m comment --comment "loopback" -j ACCEPT
-iptables -A OUTPUT -o lo -m comment --comment "loopback" -j ACCEPT
-iptables -I INPUT -i eth0 -m comment --comment "In from LAN" -j ACCEPT
-iptables -I OUTPUT -o tun+ -m comment --comment "Out to VPN" -j ACCEPT
-iptables -A OUTPUT -o eth0 -p udp --dport 1198 -m comment --comment "openvpn" -j ACCEPT
-iptables -A OUTPUT -o eth0 -p udp --dport 123 -m comment --comment "ntp" -j ACCEPT
-iptables -A OUTPUT -p UDP --dport 67:68 -m comment --comment "dhcp" -j ACCEPT
-iptables -A OUTPUT -o eth0 -p udp --dport 53 -m comment --comment "dns" -j ACCEPT
-iptables -A FORWARD -i tun+ -o eth0 -m state --state RELATED,ESTABLISHED -j ACCEPT
-iptables -A FORWARD -i eth0 -o tun+ -m comment --comment "LAN out to VPN" -j ACCEPT
-iptables -t nat -A POSTROUTING -o tun+ -j MASQUERAD
+#!/bin/bash
 
-ovpn_run
+echo 'net.ipv4.ip_forward = 1' >> /etc/sysctl.conf
+
+/usr/sbin/iptables -A INPUT -i lo -m comment --comment "loopback" -j ACCEPT
+/usr/sbin/iptables -A OUTPUT -o lo -m comment --comment "loopback" -j ACCEPT
+/usr/sbin/iptables -I INPUT -i eth0 -m comment --comment "In from LAN" -j ACCEPT
+/usr/sbin/iptables -I OUTPUT -o tun+ -m comment --comment "Out to VPN" -j ACCEPT
+/usr/sbin/iptables -A OUTPUT -o eth0 -p udp --dport 1198 -m comment --comment "openvpn" -j ACCEPT
+/usr/sbin/iptables -A OUTPUT -o eth0 -p udp --dport 123 -m comment --comment "ntp" -j ACCEPT
+/usr/sbin/iptables -A OUTPUT -p UDP --dport 67:68 -m comment --comment "dhcp" -j ACCEPT
+/usr/sbin/iptables -A OUTPUT -o eth0 -p udp --dport 53 -m comment --comment "dns" -j ACCEPT
+/usr/sbin/iptables -A FORWARD -i tun+ -o eth0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+/usr/sbin/iptables -A FORWARD -i eth0 -o tun+ -m comment --comment "LAN out to VPN" -j ACCEPT
+/usr/sbin/iptables -t nat -A POSTROUTING -o tun+ -j MASQUERAD
+
+/usr/sbin/openvpn --config /etc/openvpn/

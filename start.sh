a6e1cab7133e3422acef0ffb7049bf48324011ac
duckofdeath87 <duckofdeath87@gmail.com>
2019-12-02 19:24:49 -0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,16 +2,32 @@
 
 echo 'net.ipv4.ip_forward = 1' >> /etc/sysctl.conf
 
-/usr/sbin/iptables -A INPUT -i lo -m comment --comment "loopback" -j ACCEPT
-/usr/sbin/iptables -A OUTPUT -o lo -m comment --comment "loopback" -j ACCEPT
-/usr/sbin/iptables -I INPUT -i eth0 -m comment --comment "In from LAN" -j ACCEPT
-/usr/sbin/iptables -I OUTPUT -o tun+ -m comment --comment "Out to VPN" -j ACCEPT
-/usr/sbin/iptables -A OUTPUT -o eth0 -p udp --dport 1198 -m comment --comment "openvpn" -j ACCEPT
-/usr/sbin/iptables -A OUTPUT -o eth0 -p udp --dport 123 -m comment --comment "ntp" -j ACCEPT
-/usr/sbin/iptables -A OUTPUT -p UDP --dport 67:68 -m comment --comment "dhcp" -j ACCEPT
-/usr/sbin/iptables -A OUTPUT -o eth0 -p udp --dport 53 -m comment --comment "dns" -j ACCEPT
+# loopback
+/usr/sbin/iptables -A INPUT -i lo -j ACCEPT
+/usr/sbin/iptables -A OUTPUT -o lo -j ACCEPT
+
+# In from LAN
+/usr/sbin/iptables -I INPUT -i eth0 -j ACCEPT
+
+# Out to VPN
+/usr/sbin/iptables -I OUTPUT -o tun+ -j ACCEPT
+
+# OpenVPN
+/usr/sbin/iptables -A OUTPUT -o eth0 -p udp --dport 1198 -j ACCEPT
+
+# ntp
+/usr/sbin/iptables -A OUTPUT -o eth0 -p udp --dport 123 -j ACCEPT
+
+# dhcp
+/usr/sbin/iptables -A OUTPUT -p UDP --dport 67:68 -j ACCEPT
+
+# dns
+/usr/sbin/iptables -A OUTPUT -o eth0 -p udp --dport 53 -j ACCEPT
+
 /usr/sbin/iptables -A FORWARD -i tun+ -o eth0 -m state --state RELATED,ESTABLISHED -j ACCEPT
-/usr/sbin/iptables -A FORWARD -i eth0 -o tun+ -m comment --comment "LAN out to VPN" -j ACCEPT
+
+# LAN out to VPN
+/usr/sbin/iptables -A FORWARD -i eth0 -o tun+ -j ACCEPT
 /usr/sbin/iptables -t nat -A POSTROUTING -o tun+ -j MASQUERAD
 
 /usr/sbin/openvpn --config /etc/openvpn/vpn.conf
